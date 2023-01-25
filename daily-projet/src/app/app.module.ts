166a7b98dc168fbb4bcacaaeec9de27442645301
Abel Ngbandaman <ngbandaman2022@gmail.com>
2023-01-25 18:24:52 +0000

    final commit

--- daily-projet/src/app/app.module.ts
+++ daily-projet/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RSearchComponent } from './recipe/r-search/r-search.component';
+import { RInfoComponent } from './recipe/r-info/r-info.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RSearchComponent,
+    RInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ daily-projet/src/app/recipe/r-search/r-search.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-r-search',
+  templateUrl: './r-search.component.html',
+  styleUrls: ['./r-search.component.css']
+})
+export class RSearchComponent implements OnInit {
+  
+  // from angular material
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]> | undefined;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+}
